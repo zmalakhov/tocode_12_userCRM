be89709c9dc7c57e116e61463ddd1159a6ee644d
zmalakhov <zm.13.15.zm@gmail.com>
2020-07-02 12:13:07 +0300

    #2 - Таблица

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -3,6 +3,22 @@
         <section>
             <div class="container">
                 <h1>users page</h1>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="user in users" :key="user.id">
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.age }}</td>
+                        <td>{{ user.gender }}</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </section>
     </div>
@@ -10,6 +26,16 @@
 
 <script>
     export default {
+        data() {
+            return {
+                users: []
+            }
+        }, created() {
+            this.users = [
+                {id: 1, name: 'Jack', age: 22, gender: 'male'},
+                {id: 2, name: 'Alex', age: 24, gender: 'male'},
+            ]
+        }
     }
 </script>
 
